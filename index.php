22695b1afd1368167a7cd16b7f267829e8495c64
Santi Ciraudo <santyy7530@gmail.com>
2022-08-01 23:26:31 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <link rel="stylesheet" href="general.css">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+<div id="Contenido">
+        <div id="col-izq">
+            <h1>PC Gamer completa ryzen5 5600G 16GB 480GB SSD.</h1>
+            <img id="pc" src="pc.png" alt="PcGamer">
+        </div>
+
+    <div id="col-der">
+            <div class="info-precio-final">
+                <div class="info-icono">       
+                </div>
+                <div class="texto">
+                    <h2 id="preciofinal">$90.000<br></h2>
+                    <p>Precio lista: $105.000<br></p>
+                </div>
+            </div>
+            <div class="info-preciocuotas">
+                <div class="icono">
+                    <img class="icono" src="cuotas.png" alt="cuotasDisponibles">
+                </div>
+                <div class="texto">
+                    <p>12 cuotas fijas de $8750 a precio de lista<br></p>
+                </div>
+            </div>
+            <div class="info-Stock">
+                <div class="icono">
+                    <img class="icono" src="stock.png" alt="StockDisponible">
+                </div>
+                <div class="texto">
+                    <p>Actualmente con Stock<br></p>
+                </div>
+            </div>
+            <div class="info-Oferta">
+                <div class="icono">
+                    
+                </div>
+                <div class="texto">
+                    <p>Oferta con la compra de un monitor $110000<br></p>
+                </div>
+            </div>
+    </div>
+</div>
+    </body>
+</html>
